4a256dc7199ee31bb36658f0fb4fc3a6ba494f02
Vint Dave <112562574+vincentdavidson@users.noreply.github.com>
2022-11-11 21:15:17 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,12 @@ export default function Home() {
           <code className={styles.code}>vadavid</code>
         </p>
 
+        <p className={styles.description}>
+
+          <a href="chess" className={styles.card}>Lets Play Chess</a>
+
+        </p>
+
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h2>Documentation &rarr;</h2>
